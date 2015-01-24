7aab0f84f91ce29591435bc5b5d1d818d22687c1
Sven Eberth <Sven.Eberth@gmail.com>
2015-01-24 22:55:49 +0100

    started with design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	console.log('hey!');
+	
+	var category = 
+	{
+		'Maths': ['area', 'volume'],
+		'Phsics': ['power', 'weight'],
+	}
+	
+	// category accordion
+    $("menu.category").accordion({
+		 icons: false,
+	});
+	
+
+	$("menu.category ul li").bind('click', function() {
+		
+	});
+	
 });
