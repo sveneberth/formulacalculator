bc54a1799b001236c1a417e686402ae718b50af5
Sven Eberth <Sven.Eberth@gmail.com>
2015-01-24 18:43:47 +0100

    removed google font

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
 $(document).ready(function() {
-	notifyMe('hey!');
+	console.log('hey!');
 });
-
-function notifyMe(title, text, click) {
-	if(!Notification) {
-		console.error('Notification are not supported in this browser');
-		return false;
-	}
-
-	if (Notification.permission !== "granted")
-		Notification.requestPermission();
-
-	var notification = new Notification(title, {
-	icon: $('link[rel="shortcut icon"]').attr('href'),
-	body: text,
-	});
-
-	notification.onclick = click;
-}
