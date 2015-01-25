05df20e654240e2649c77fa9ce35bda79118252f
Sven Eberth <Sven.Eberth@gmail.com>
2015-01-25 21:51:05 +0100

    further made

---
+++ data/data.js
@@ -0,0 +1,19 @@
+var category = 
+{
+	'Maths': ['Area', 'Volume', /*'Length' length != formula */],
+	'Physics': ['Power', /*'Weight' Weight != formula*/],
+};
+
+var formula =
+{
+	'Area':
+	{
+		'scuare': '[height] ^ 2',
+		'rectangle': '[height] * [width]',
+	},
+	'Volume':
+	{
+		'cube': '[height] ^ 3',
+		'cuboid': '[height] * [width] * [deepth]',
+	},
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 	
-	var category = 
-	{
-		'Maths': ['Area', 'Volume', 'Length'],
-		'Phsics': ['Power', 'Weight'],
-	}
-	
 	/* build category menu */
 	function buildcategorymenu(callback) {
 		for(key in category) {
@@ -27,11 +21,36 @@ $(document).ready(function() {
 		});
 	})
 	
+	function fillcontent(section) {
+		for(key in formula[section]) {
+		
+			$('section.' + section).append('<h3>' + key + ':</h3>');
+				
+			var str = formula[section][key].replace(/\[(\w+)\]/g, function replacer(all, match){
+				return '<input style="margin: 0;display:inline-block;width:100px;" name="' + key + '_' + match + '" type="number" placeholder="' + match + '" />';
+			});
+			
+			$('section.' + section).append(str);
+			
+		}
+	}
+	
 
 	$("menu.category ul li").bind('click', function() {
 		var val = $(this).text();
 		console.log("clicked on \"%s\"", val);
-
+				
+		if($('section.' + val).length == 0) {
+			$('.wrapper').append('<section class="' + val + '"><h1>' + val + '</h1></section>');
+			fillcontent(val);
+		}
+		
+		// FIXME: if created new section
+		$('section').not($('section.' + val)).filter(':visible').fadeOut(300, function() {
+			$('section.' + val).fadeIn(300);
+		});
+		
+		
 	});
 	
 });
