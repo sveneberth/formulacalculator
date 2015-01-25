c13063f16d50a21ed06df38f79025e563a984687
Sven Eberth <Sven.Eberth@gmail.com>
2015-01-25 18:33:01 +0100

    further made

--- js/script.js
+++ js/script.js
@@ -2,18 +2,36 @@ $(document).ready(function() {
 	
 	var category = 
 	{
-		'Maths': ['area', 'volume'],
-		'Phsics': ['power', 'weight'],
+		'Maths': ['Area', 'Volume', 'Length'],
+		'Phsics': ['Power', 'Weight'],
 	}
 	
-	// category accordion
-    $("menu.category").accordion({
-		 icons: false,
-	});
+	/* build category menu */
+	function buildcategorymenu(callback) {
+		for(key in category) {
+			var data = '<h2>' + key + '</h2><ul>';
+			for(val of category[key]) {
+				data += '<li>' + val + '</li>';
+			}
+			data += '</ul>';
+			$('menu.category').append(data);
+		}
+		callback();
+	}
+	
+	/* run build category menu with callback */
+    buildcategorymenu(function() {
+		// category accordion
+		$("menu.category").accordion({
+			icons: false,
+		});
+	})
 	
 
 	$("menu.category ul li").bind('click', function() {
-		
+		var val = $(this).text();
+		console.log("clicked on \"%s\"", val);
+
 	});
 	
 });
